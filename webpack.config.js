e1af6b2d817069b43a0e5f59e440f31c300d0418
Purushotthama B V <purushothamacharya@gmail.com>
2023-08-02 09:19:00 +0530

    root

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,21 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
-    entry: './src/main.ts', // Entry point of your Angular application
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'my-angular-app.bundle.js', // Output file name
-    },
-    resolve: {
-        extensions: ['.ts', '.js'], // Add other extensions if needed
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000,
-    },
+  entry: '../use-cdn/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'chatbot.bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
